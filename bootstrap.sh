407ff829b1a62a5324c90696da227e116031406b
NathanSmela <nsmela@hotmail.com>
2026-05-07 06:56:10 -0700

    feat: add cleanup step to remove existing installation before deployment in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,14 +15,20 @@ GITHUB_URL="https://github.com/nsmela/qatrackplus-manager"
 apt-get update -qq
 apt-get install -y -qq python3 python3-pip python3-venv
 
-# 4. Create manager venv (separate from the QA Track venv)
+# 4. Clear existing installation
+echo "Cleaning up any existing installation..."
+rm -rf "${MANAGER_VENV}"
+rm -f /usr/local/sbin/qatrackplus-manager
+
+# 5. Create manager venv (separate from the QA Track venv)
 python3 -m venv "${MANAGER_VENV}"
 
-# 5. Install package from GitHub
+
+# 6. Install package from GitHub
 "${MANAGER_VENV}/bin/pip" install --upgrade pip wheel
 "${MANAGER_VENV}/bin/pip" install "git+${GITHUB_URL}.git"
 
-# 6. Create convenience wrapper
+# 7. Create convenience wrapper
 cat > /usr/local/sbin/qatrackplus-manager << EOF
 #!/bin/bash
 exec "${MANAGER_VENV}/bin/qatrackplus-manager" "\$@"
